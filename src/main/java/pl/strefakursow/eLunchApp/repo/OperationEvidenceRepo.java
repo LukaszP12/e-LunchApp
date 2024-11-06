9510c3aee62e80fb13d79dc47802c02a8ada21a8
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-11-06 17:03:10 +0100

    added query needed from suming total value of operation of given users, minor corrections additionally.

--- src/main/java/pl/strefakursow/eLunchApp/repo/OperationEvidenceRepo.java
+++ src/main/java/pl/strefakursow/eLunchApp/repo/OperationEvidenceRepo.java
@@ -1,12 +1,28 @@
 package pl.strefakursow.eLunchApp.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pl.strefakursow.eLunchApp.model.OperationEvidence;
+import pl.strefakursow.eLunchApp.model.User;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface OperationEvidenceRepo extends JpaRepository<OperationEvidenceRepo, Long> {
+public interface OperationEvidenceRepo extends JpaRepository<OperationEvidence, Long> {
+
     Optional<OperationEvidenceRepo> findByUUID(UUID uuid);
+
+    @Query("SELECT COALESCE(SUM(CASE WHEN e.type = pl.strefakursow.eLunchApp.model.enums.EvidenceType.DEPOSIT THEN e.amount" +
+            "WHEN e.type = pl.strefakursow.eLunchApp.model.enums.EvidenceType.WITHDRAW" +
+            "or pl.strefakursow.eLunchApp.model.enums.EvidenceType.PAYMENT THEN -e.amount" +
+            "ELSE 0 " +
+            "END" +
+            "),0) from operationevidence e" +
+            "WHERE e.user =: user"
+    )
+    BigDecimal getUserAccountBalance(@Param("user") User user);
 }
