bec7ebb651054534b9ef3d07933b8c0aba3b58fd
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-11-07 10:54:45 +0100

    interfaces of service classes

---
+++ src/main/java/pl/strefakursow/eLunchApp/service/DelivererService.java
@@ -0,0 +1,17 @@
+package pl.strefakursow.eLunchApp.service;
+
+import pl.strefakursow.eLunchApp.DTO.DelivererDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface DelivererService {
+    List<DelivererDTO> getAll();
+
+    void put(UUID uuid, DelivererDTO delivererDTO);
+
+    void delete(UUID uuid);
+
+    Optional<DelivererDTO> getByUUID(UUID uuid);
+}
