3ffa3bc3b74d89a86c4700d67377c15ecceb3264
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-11-07 13:40:44 +0100

    added more draft controller classes returning output in json format

---
+++ src/main/java/pl/strefakursow/eLunchApp/controller/MenuItemController.java
@@ -0,0 +1,50 @@
+package pl.strefakursow.eLunchApp.controller;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.strefakursow.eLunchApp.DTO.MenuItemDTO;
+import pl.strefakursow.eLunchApp.service.MenuItemService;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/menu-item", produces = APPLICATION_JSON_VALUE)
+public class MenuItemController {
+
+    private final MenuItemService menuItemService;
+
+    public MenuItemController(MenuItemService menuItemService) {
+        this.menuItemService = menuItemService;
+    }
+
+    @GetMapping
+    public List<MenuItemDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public MenuItemDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public void put(@PathVariable UUID uuid, @RequestBody MenuItemDTO delivererJson) {
+
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public void delete(@PathVariable UUID uuid) {
+
+    }
+}
