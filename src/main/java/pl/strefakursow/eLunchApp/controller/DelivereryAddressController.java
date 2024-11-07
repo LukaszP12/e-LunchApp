fd42324f93d7414ebb5d61330cd356c60aa3d4a1
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-11-07 13:29:52 +0100

    added controller classes returning output in json format

---
+++ src/main/java/pl/strefakursow/eLunchApp/controller/DelivereryAddressController.java
@@ -0,0 +1,50 @@
+package pl.strefakursow.eLunchApp.controller;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.strefakursow.eLunchApp.DTO.DeliveryAddressDTO;
+import pl.strefakursow.eLunchApp.service.DeliveryAddressService;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/delivery-address", produces = APPLICATION_JSON_VALUE)
+public class DelivereryAddressController {
+
+    private final DeliveryAddressService deliveryAddressService;
+
+    public DelivereryAddressController(DeliveryAddressService deliveryAddressService) {
+        this.deliveryAddressService = deliveryAddressService;
+    }
+
+    @GetMapping
+    public List<DeliveryAddressDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public DeliveryAddressDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public void put(@PathVariable UUID uuid, @RequestBody DeliveryAddressDTO json) {
+
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public void delete(@PathVariable UUID uuid) {
+
+    }
+}
