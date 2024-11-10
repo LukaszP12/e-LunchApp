425ef0cb463d8129c0ddf59bd8dcee42b055cf88
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-11-10 11:57:57 +0100

    PUT method implementation of DelivererService including caching mechanism

--- src/main/java/pl/strefakursow/eLunchApp/model/Deliverer.java
+++ src/main/java/pl/strefakursow/eLunchApp/model/Deliverer.java
@@ -3,11 +3,13 @@ package pl.strefakursow.eLunchApp.model;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import net.karneim.pojobuilder.GeneratePojoBuilder;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
 
+@GeneratePojoBuilder
 @Entity
 @DiscriminatorValue("deliverer")
 public class Deliverer extends Employee {
--- src/main/java/pl/strefakursow/eLunchApp/service/DelivererServiceImpl.java
+++ src/main/java/pl/strefakursow/eLunchApp/service/DelivererServiceImpl.java
@@ -1,48 +1,97 @@
 package pl.strefakursow.eLunchApp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import pl.strefakursow.eLunchApp.DTO.DelivererDTO;
+import pl.strefakursow.eLunchApp.DTO.OrderDTO;
 import pl.strefakursow.eLunchApp.model.Deliverer;
+import pl.strefakursow.eLunchApp.model.DelivererBuilder;
+import pl.strefakursow.eLunchApp.model.Order;
 import pl.strefakursow.eLunchApp.repo.DelivererRepo;
 import pl.strefakursow.eLunchApp.repo.OrderRepo;
 import pl.strefakursow.eLunchApp.utils.ConverterUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-@Service
-public class DelivererServiceImpl implements DelivererService {
+import static pl.strefakursow.eLunchApp.utils.ConverterUtils.convert;
 
+@Service
+@CacheConfig(cacheNames = "deliverers")
+public class DelivererServiceImpl implements DelivererService {
     private final DelivererRepo delivererRepo;
     private final OrderRepo orderRepo;
 
+    @Autowired
     public DelivererServiceImpl(DelivererRepo delivererRepo, OrderRepo orderRepo) {
         this.delivererRepo = delivererRepo;
         this.orderRepo = orderRepo;
     }
 
+    @Cacheable(cacheNames = "deliverers")
     @Override
     public List<DelivererDTO> getAll() {
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return delivererRepo.findAll()
                 .stream()
                 .map(ConverterUtils::convert)
                 .collect(Collectors.toList());
     }
 
+    @CacheEvict(cacheNames = "deliverers", allEntries = true)
     @Override
     public void put(UUID uuid, DelivererDTO delivererDTO) {
+        if (!Objects.equals(delivererDTO.getUuid(), uuid)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
+        Deliverer deliverer = delivererRepo.findByUUID(delivererDTO.getUuid())
+                .orElseGet(() -> newDeliverer(delivererDTO.getUuid()));
+
+        List<Order> orders = new ArrayList<>();
+        if (delivererDTO.getOrdersDtos() != null) {
+            for (OrderDTO o : delivererDTO.getOrdersDtos()) {
+                Order order = orderRepo.findByUUID(o.getUuid())
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                orders.add(order);
+            }
+        }
+
+        deliverer.setPersonalData(convert(delivererDTO.getPersonalData()));
+        deliverer.setLoginData(convert(delivererDTO.getLoginData()));
+        deliverer.setArchive(delivererDTO.getArchive());
+        deliverer.setOrders(orders);
+
+        if (deliverer.getId() == null) {
+            delivererRepo.save(deliverer);
+        }
     }
 
+    private Deliverer newDeliverer(UUID uuid) {
+        return new DelivererBuilder()
+                .withUuid(uuid)
+                .build();
+    }
+
+    @CacheEvict(cacheNames = "deliverers", allEntries = true)
     @Override
     public void delete(UUID uuid) {
         Deliverer deliverer = delivererRepo.findByUUID(uuid)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        delivererRepo.deleteById(deliverer.getId());
+        delivererRepo.delete(deliverer);
     }
 
     @Override
